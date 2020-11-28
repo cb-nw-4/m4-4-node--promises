03e9ff20eba11329beb89a3e0854ad5a2e203ca2
Gia Dat Doan <giadatdoan@gmail.com>
2020-11-28 13:49:59 -0500

    3.1- done

--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -2,8 +2,17 @@
 // -----------------------------
 
 // require the 'request-promise' module.
+const request = require('request-promise');
 
 // Returns the current position of the ISS
 const getIssPosition = () => {
-  // write some code...
+  return request('http://api.open-notify.org/iss-now.json')
+    .then((response) => JSON.parse(response))
+    .then((parsedResponse) => {
+      // console.log(parsedResponse);
+      return {lat: parsedResponse.iss_position.longitude, lng: parsedResponse.iss_position.latitude};
+    })
 };
+
+// Testing
+getIssPosition().then((result) => console.log(result, 'result'));
