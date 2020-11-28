57809cfe69b7691d5a98e2db63c5c9ecaf9c8b71
chris shino <chris.shino@hotmail.com>
2020-11-28 13:17:51 -0500

    done 1 + 2.1

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -1,14 +1,34 @@
 // Exercise 1
 // ------------
-const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
-const complicatedArray = ['cucumber', 44, true];
+const arrayOfWords = ["cucumber", "tomatos", "avocado"];
+const complicatedArray = ["cucumber", 44, true];
 
 const makeAllCaps = (array) => {
   // write some code
+  myArray = new Promise((resolve, reject) => {
+    if (array.every(el => typeof el === 'string')) {
+      resolve(
+        array.map((el) => {
+          return el.toUpperCase();
+        })
+      );
+    } else {
+      reject("error");
+    }
+  });
+  return myArray;
 };
 
 const sortWords = (array) => {
   // write some code
+  sortedArray = new Promise ((resolve,reject) => {
+    if (array.every(el => typeof el === 'string')) {
+      resolve(array.sort())
+    } else {
+      reject('not all strings')
+    }
+  })
+  return sortedArray
 };
 
 // Calling (testing)
