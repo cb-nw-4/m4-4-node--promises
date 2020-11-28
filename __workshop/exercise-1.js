fbf09650366296a8863fd4fb9186dff6d6143d03
Zayd Ben Osmane <zayd@benosmane.com>
2020-11-28 12:32:34 -0500

    100% done

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -4,12 +4,30 @@ const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
 const complicatedArray = ['cucumber', 44, true];
 
 const makeAllCaps = (array) => {
-  // write some code
+  myPromise = new Promise((resolve, reject) => {
+    if (array.every((item) => typeof item === "string")) {
+      const capitalized = array.map((e) => e.toUpperCase());
+      resolve(capitalized);
+    } else {
+      reject(`error`);;
+    }
+  });
+  return myPromise;
 };
 
 const sortWords = (array) => {
   // write some code
+  prom = new Promise((resolve, reject) => {
+    if (array.every((item) => typeof item === "string")) {
+      const sorted = array.sort();
+      resolve(sorted);
+    } else {
+      reject(`error`);;
+    }
+  });
+  return prom;
 };
+
 
 // Calling (testing)
 makeAllCaps(arrayOfWords)
