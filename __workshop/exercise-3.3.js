b2fdd3ac8d56215b927132bdc7e224f55baa22e7
chris shino <chris.shino@hotmail.com>
2020-11-28 15:13:52 -0500

    done 3.2

--- __workshop/exercise-3.3.js
+++ __workshop/exercise-3.3.js
@@ -1,18 +1,19 @@
 // Exercise 3.3 - `getAddressPosition`
 // ---------------------------------
 
-const opencage = require('opencage-api-client');
-require('dotenv').config();
+const opencage = require("opencage-api-client");
+require("dotenv").config();
 
 function getAddressFromPosition(lat, lng) {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: process.env.OPENCAGE_API_KEY,
+    q: "",
   };
-
+  console.log(requestObj);
   // return ...
+  return true;
 }
 
-getAddressFromPosition('48.8584', '2.2945').then((response) =>
+getAddressFromPosition("48.8584", "2.2945").then((response) =>
   console.log(response)
 );
