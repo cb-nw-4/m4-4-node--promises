27311353fa779a5dbf652da6014d57b004f90640
“Winnie <chanwinnie.wc@gmail.com>
2020-11-28 14:05:55 -0500

    Exercise 1

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -1,14 +1,29 @@
 // Exercise 1
 // ------------
-const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
+const arrayOfWords = ['cucumber', 'tomatoes', 'avocado'];
 const complicatedArray = ['cucumber', 44, true];
 
 const makeAllCaps = (array) => {
-  // write some code
+  let isString = array.every(item => typeof item === "string");
+  makeCapsPromise = new Promise((resolve,reject) => {
+    if (isString) {
+      resolve(array.map(item => item.toUpperCase()));
+    } else {
+      reject("Error: array contains non-strings.")
+    }
+  })
+  return makeCapsPromise;
 };
 
 const sortWords = (array) => {
-  // write some code
+  sortWordsPromise = new Promise((resolve,reject) =>{
+    if(makeCapsPromise) {
+      resolve(`Success: [${array.sort()}].`)
+    } else {
+      reject("Error")
+    }
+  })
+  return sortWordsPromise;
 };
 
 // Calling (testing)
@@ -21,3 +36,5 @@ makeAllCaps(complicatedArray)
   .then(sortWords)
   .then((result) => console.log(result))
   .catch((error) => console.log(error));
+
+
