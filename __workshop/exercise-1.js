6488ecfb1f1e131c99cae5c87a9851bef170e604
Sandra <sandrabess@hotmail.com>
2020-11-28 16:28:35 -0500

    exercise 1 completed

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -3,15 +3,38 @@
 const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
 const complicatedArray = ['cucumber', 44, true];
 
-const makeAllCaps = (array) => {
-  // write some code
+const makeAllCaps = (array) => { 
+   mypromise = new Promise((resolve, reject) => {
+      const allCapArr = array.map((word) => {
+        if (!(typeof word === 'string' || word instanceof String))
+          reject('All element in the array should be a string!, error!');       
+        return word.toUpperCase();
+      });
+   
+      resolve(allCapArr);    
+   });
+
+   return mypromise;
 };
 
 const sortWords = (array) => {
-  // write some code
+   mypromise = new Promise((resolve, reject) => {
+      if (!array.every((word) =>(typeof word === 'string' || word instanceof String)))
+        reject('All element in the array should be a string!, error!');
+
+      let sortedArr = array.sort(function(a,b){
+        if(a.toLowerCase() < b.toLowerCase()) { return -1; }
+        if(a.toLowerCase() > b.toLowerCase()) { return 1; }
+        return 0;
+      });     
+ 
+    resolve(sortedArr);
+   });
+   return mypromise;
 };
 
 // Calling (testing)
+
 makeAllCaps(arrayOfWords)
   .then(sortWords)
   .then((result) => console.log(result))
