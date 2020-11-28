511678cc427428d3ed752096b27b75564d2cf6d0
ferielnabet <fnabet@ipconnex.com>
2020-11-28 14:20:00 -0500

    exercise 2 Done

--- __workshop/exercise-2.2.js
+++ __workshop/exercise-2.2.js
@@ -5,15 +5,22 @@
 const request = require('request-promise');
 
 const greeting = (langCode) => {
-  return request('<URL>') // 1
-    .then((response) => JSON.parse(response))
+  return request(`https://journeyedu.herokuapp.com/hello/${langCode}`) // 1
+  
+    .then((response) => JSON.parse(response)) 
     .then((parsedResponse) => {
-      return; // 2
+      return parsedResponse.data; // 2
     })
     .catch((err) => console.log('Error: ', err));
 };
 
+
+
 // Testing
 greeting('fr').then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
 
+
 // 3
+
+greeting('es').then((result) => console.log(result));
+greeting('ar').then((result) => console.log(result));
