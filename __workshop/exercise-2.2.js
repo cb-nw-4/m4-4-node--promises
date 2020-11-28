34fafff280b3ea49219d737918a0a2b2b11538e0
“Boris <your@email.comBoris>
2020-11-28 14:33:55 -0500

    Up to 2.3 Complete

--- __workshop/exercise-2.2.js
+++ __workshop/exercise-2.2.js
@@ -5,15 +5,19 @@
 const request = require('request-promise');
 
 const greeting = (langCode) => {
-  return request('<URL>') // 1
+  return request(`https://journeyedu.herokuapp.com/hello/${langCode}`) // 1
     .then((response) => JSON.parse(response))
     .then((parsedResponse) => {
-      return; // 2
+      return parsedResponse.data; // 2
     })
     .catch((err) => console.log('Error: ', err));
 };
 
 // Testing
 greeting('fr').then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
-
+greeting('en').then((result) => console.log(result)); // English
+greeting('es').then((result) => console.log(result)); // Spanish
+greeting('sw').then((result) => console.log(result)); // Swahili
+greeting('fi').then((result) => console.log(result)); // Finnish
+greeting('ru').then((result) => console.log(result)); // Russian
 // 3
