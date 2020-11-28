d71f37eade82b4854a2f447f40c6e3b3d002dec3
Gia Dat Doan <giadatdoan@gmail.com>
2020-11-28 13:20:22 -0500

    exercise-2.1

--- __workshop/exercise-2.1.js
+++ __workshop/exercise-2.1.js
@@ -5,12 +5,14 @@
 const request = require('request-promise');
 
 const testGreeting = () => {
-  return request('<API_URL') // 1
+  return request('https://journeyedu.herokuapp.com/hello') // 1
     .then((response) => {
-      return; // 2
+      console.log(response)
+      return JSON.parse(response); // 2
     })
     .then((parsedResponse) => {
-      return; // 3
+      console.log(parsedResponse);
+      return parsedResponse.data.text; // 3
     })
     .catch((err) => console.log('Error: ', err));
 };
