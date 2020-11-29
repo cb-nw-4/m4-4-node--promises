09de95348e742f3c91640f3f4e3a3ceee2b3b11c
Joni Sadler <joni.anne.sadler@gmail.com>
2020-11-29 10:33:25 -0500

    Exercise 2.2 done

--- __workshop/exercise-2.2.js
+++ __workshop/exercise-2.2.js
@@ -5,15 +5,19 @@
 const request = require('request-promise');
 
 const greeting = (langCode) => {
-  return request('<URL>') // 1
-    .then((response) => JSON.parse(response))
-    .then((parsedResponse) => {
-      return; // 2
-    })
-    .catch((err) => console.log('Error: ', err));
+  return request(`https://journeyedu.herokuapp.com/hello/${langCode}`) // 1
+  .then((response) => {
+    return parsedResponse = JSON.parse(response);
+  })
+  .then((parsedResponse) => {
+    return parsedResponse.data; // 2
+  })
+  .catch((err) => console.log('Error: ', err));
 };
 
 // Testing
 greeting('fr').then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
-
+greeting('en').then((result) => console.log(result));
+greeting('de').then((result) => console.log(result));
+greeting('ru').then((result) => console.log(result));
 // 3
