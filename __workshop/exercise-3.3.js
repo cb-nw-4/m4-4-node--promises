d48874d4ad683f6bf3b087cfd0f031a37b47a3a4
ayasatograph@icloud.com <ayasatography@icloud.com>
2020-11-29 17:07:45 -0500

    Exercise-3.3 completed

--- __workshop/exercise-3.3.js
+++ __workshop/exercise-3.3.js
@@ -6,11 +6,18 @@ require('dotenv').config();
 
 function getAddressFromPosition(lat, lng) {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    q: `${lat}, ${lng}`,
+    key: process.env.OPENCAGE_API_KEY
   };
-
-  // return ...
+  return opencage
+    .geocode(requestObj)
+    .then((data) => {
+      const response = data.results[0].formatted;
+      return response;
+    })
+    .catch((error) => {
+      console.log('error', error.message);
+    });
 }
 
 getAddressFromPosition('48.8584', '2.2945').then((response) =>
