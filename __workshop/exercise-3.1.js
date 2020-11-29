4f02b202db05852dd64a96091fe349ba09b12af6
ayasatograph@icloud.com <ayasatography@icloud.com>
2020-11-29 16:53:26 -0500

    Exercise-3.1 completed

--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -2,8 +2,21 @@
 // -----------------------------
 
 // require the 'request-promise' module.
+const request = require('request-promise');
 
 // Returns the current position of the ISS
 const getIssPosition = () => {
-  // write some code...
+  return request('http://api.open-notify.org/iss-now.json')
+    .then((response) => {
+      const parsedResponse = JSON.parse(response);
+      return parsedResponse;
+    })
+    .then((parsedResponse) => {
+      let result = {lat: parsedResponse.iss_position.latitude, lng: parsedResponse.iss_position.longitude};
+      return result;
+    })
+    .catch((err) => console.log('Error: ', err));
 };
+
+//Testing
+getIssPosition().then((result) => console.log(result)); 
