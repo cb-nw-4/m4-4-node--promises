d360eb08bc04152c2e7d8ef051fd983d04119603
Joni Sadler <joni.anne.sadler@gmail.com>
2020-11-29 21:05:54 -0500

    Exercises 3.2 and 3.3 done

--- __workshop/exercise-3.2.js
+++ __workshop/exercise-3.2.js
@@ -2,17 +2,29 @@
 // ---------------------------------
 
 const opencage = require('opencage-api-client');
+const request = require('request-promise');
 require('dotenv').config();
 
 const getPositionFromAddress = (address) => {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: process.env.OPENCAGE_API_KEY,
+    q: address,
   };
-
-  // return something...
+  return opencage.geocode(requestObj)
+  .then((response) => {
+    newObject = {};
+    newObject.lat = response.results[0].geometry.lat;
+    newObject.lng = response.results[0].geometry.lng;
+    return newObject;
+  })
+  .catch((err) => {
+    console.log(err);
+    return err.error ? JSON.parse(err.error) : err;
+  })
 };
 
 getPositionFromAddress(
   '1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8'
 ).then((response) => console.log(response));
+
+
--- __workshop/exercise-3.3.js
+++ __workshop/exercise-3.3.js
@@ -2,17 +2,25 @@
 // ---------------------------------
 
 const opencage = require('opencage-api-client');
+const request = require('request-promise');
 require('dotenv').config();
 
 function getAddressFromPosition(lat, lng) {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: process.env.OPENCAGE_API_KEY,
+    q: `${lat} ${lng}`,
   };
-
-  // return ...
-}
+  return opencage.geocode(requestObj)
+  .then((response) => {
+    return response.results[0].formatted;
+  })
+  .catch((err) => {
+    console.log(err);
+    return err.error ? JSON.parse(err.error) : err;
+  })
+};
 
 getAddressFromPosition('48.8584', '2.2945').then((response) =>
   console.log(response)
 );
+
