0ea81bb46584623f5e0eef8c05e3290269c2d2b4
Sandra <sandrabess@hotmail.com>
2020-11-30 16:56:03 -0500

    exercise 3 completed

--- __workshop/exercise-3.2.js
+++ __workshop/exercise-3.2.js
@@ -6,13 +6,29 @@ require('dotenv').config();
 
 const getPositionFromAddress = (address) => {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: process.env.OPENCAGE_API_KEY,
+    q: address,
   };
-
-  // return something...
+  return opencage
+    .geocode(requestObj)
+    .then((data) => (data.results[0].geometry))  
+    .catch((error) => {
+       return error.message;
+    });
+    
 };
 
 getPositionFromAddress(
   '1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8'
-).then((response) => console.log(response));
+).then((response) => console.log("Exercise 3-2:", response));
+
+
+getPositionFromAddress(
+  '152, 61e avenue Nord, Saint-Joseph-du-lac, QC J0N1M0'
+).then((response) => console.log("Exercise 3-2:", response));
+
+getPositionFromAddress(
+  '5800 Saint Denis St, Montreal, QC H2S 3L5'
+).then((response) => console.log("Exercise 3-2:", response));
+
+module.exports = { getPositionFromAddress };
--- __workshop/exercise-3.4.js
+++ __workshop/exercise-3.4.js
@@ -1,6 +1,26 @@
 // Exercise 3.4 - `getDistanceFromIss`
 // ----------------------------------
+const { getPositionFromAddress } = require('./exercise-3.2');
+const { getIssPosition } = require('./exercise-3.1');
+
+// Euclidian distance between two points
+const getDistance = (pos1, pos2) => {
+  return Math.sqrt(
+    Math.pow(pos1.lat - pos2.lat, 2) + Math.pow(pos1.lng - pos2.lng, 2)
+  );
+};
 
 const getDistanceFromIss = (address) => {
-  // write a new Promise here...
+  
+  return getPositionFromAddress(address)
+    .then((positionAdresse) =>{
+       return getIssPosition()
+          .then((issPosition) =>{          
+             return getDistance(positionAdresse, issPosition);
+          });
+    })    
+    .catch((err) => err);  
 };
+
+getDistanceFromIss('1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8').then((response) =>
+console.log("Exercise 3-4:", response));
