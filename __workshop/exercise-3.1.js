30fb92322118b5309d4b4d9909a103df356736b4
Vishal Handa <70589573+vishal-handa@users.noreply.github.com>
2020-12-01 09:49:35 -0500

    till 3.1

--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -2,8 +2,20 @@
 // -----------------------------
 
 // require the 'request-promise' module.
-
+const request = require('request-promise');
 // Returns the current position of the ISS
 const getIssPosition = () => {
-  // write some code...
+  return request('http://api.open-notify.org/iss-now.json')
+  .then((response) => JSON.parse(response))
+  .then((parsedResponse)=>{
+    let lat=parsedResponse.iss_position.latitude;
+    let long=parsedResponse.iss_position.longitude;
+    let obj={"lat":lat,"long":long};
+    console.log(obj);
+    return;
+  })
+  .catch((error)=>console.log(error));
+
 };
+
+getIssPosition().then((result)=>console.log(result));
