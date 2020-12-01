39079e37d850d6dca1b35c0c786fd230070c6e6f
Mallory Belgica <mallorybelgica@gmail.com>
2020-11-30 22:10:33 -0500

    Completed node-promisees 75%

--- __workshop/exercise-3.3.js
+++ __workshop/exercise-3.3.js
@@ -6,12 +6,19 @@ require('dotenv').config();
 
 function getAddressFromPosition(lat, lng) {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: process.env.OPENCAGE_API_KEY,
+    q: lat, lng,
   };
 
   // return ...
-}
+  return opencage
+  .geocode(requestObj)
+  .then((data) => {
+    let address = data.results[0]
+    return address.formatted;
+  })
+  .catch((err) => console.log('Error: ', err));  
+  }
 
 getAddressFromPosition('48.8584', '2.2945').then((response) =>
   console.log(response)
