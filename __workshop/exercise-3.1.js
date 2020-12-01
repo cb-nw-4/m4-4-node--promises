b6171de66a4b560e77dcea3fd24417c279dc5fc0
Elodie L <67484805+EL-Bloom@users.noreply.github.com>
2020-12-01 15:25:14 -0500

    Finished 100% (3.4)

--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -1,9 +1,23 @@
 // Exercise 3.1 - `getIssPosition`
 // -----------------------------
 
-// require the 'request-promise' module.
+const request = require('request-promise');
 
 // Returns the current position of the ISS
 const getIssPosition = () => {
-  // write some code...
+  return request("http://api.open-notify.org/iss-now.json") // 1
+  .then((response) => JSON.parse(response))
+  .then((parsedResponse) => {
+  let position = parsedResponse.iss_position; 
+ let lat = position.latitude;
+ let lng = position.longitude;
+ return {lng, lat};
+  })
+  .catch((err) => {
+    return err.error ? JSON.parse(err.error) : err;
+  }); 
 };
+
+getIssPosition().then((result) => console.log(result));
+
+module.exports = { getIssPosition };
