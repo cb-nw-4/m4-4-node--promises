5a4d1d4664e0bb6e6aaaa08dfdd839a4121d08d7
Vanessa Chan <chw.vanessa@gmail.com>
2020-12-01 20:43:31 -0500

    Ex 3.1

--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -2,8 +2,16 @@
 // -----------------------------
 
 // require the 'request-promise' module.
+const request = require('request-promise'); 
 
 // Returns the current position of the ISS
 const getIssPosition = () => {
-  // write some code...
+  return request("http://api.open-notify.org/iss-now.json")
+  .then((response)=>JSON.parse(response))
+  .then((parsedRes)=>
+      console.log('lat: '+ parsedRes.iss_position.latitude, 
+      'lng: '+ parsedRes.iss_position.longitude))
+  .catch((err)=>console.log(err));
 };
+
+getIssPosition();
