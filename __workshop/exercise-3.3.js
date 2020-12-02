2dc16a84f74d96dad946362ddd220c3f73c28fc3
Vanessa Chan <chw.vanessa@gmail.com>
2020-12-01 21:36:01 -0500

    Ex 3.3

--- __workshop/exercise-3.3.js
+++ __workshop/exercise-3.3.js
@@ -1,18 +1,24 @@
 // Exercise 3.3 - `getAddressPosition`
 // ---------------------------------
 
+const { geocode } = require('opencage-api-client');
 const opencage = require('opencage-api-client');
 require('dotenv').config();
 
 function getAddressFromPosition(lat, lng) {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: '51ed2a8b0e4c437f9d8941eda6619cba',
+    q: lat+ " " + lng,
   };
 
   // return ...
+  return opencage
+  .geocode(requestObj)
+  .then((data)=>data.results[0].formatted)
+  .catch((err)=>err.message);
 }
 
 getAddressFromPosition('48.8584', '2.2945').then((response) =>
+  //console.dir(response,{depth:null})
   console.log(response)
 );
