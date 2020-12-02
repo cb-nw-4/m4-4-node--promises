36a2dad7224ee32ead01c7bae0fed11029a5645b
Alexandre <alex_18h@hotmail.com>
2020-12-01 20:28:37 -0500

    exercise 2.1

--- __workshop/exercise-2.1.js
+++ __workshop/exercise-2.1.js
@@ -5,12 +5,12 @@
 const request = require('request-promise');
 
 const testGreeting = () => {
-  return request('<API_URL') // 1
+  return request('https://journeyedu.herokuapp.com') // 1
     .then((response) => {
-      return; // 2
+      return JSON.parse(response);
     })
     .then((parsedResponse) => {
-      return; // 3
+      return parsedResponse.data.text
     })
     .catch((err) => console.log('Error: ', err));
 };
